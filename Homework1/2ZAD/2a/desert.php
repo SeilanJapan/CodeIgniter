9f63fb99052ecd04c67c2a596991465f89a2fd75
Petya Ilieva <pipion@abv.bg>
2015-11-01 03:50:03 +0200

    Create desert.php

---
+++ Homework1/2ZAD/2a/desert.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+  Като спазвате конвенциите за писане на PHP си направете контролер, 
+  който има следните методи:
+  а) метод, който отпечатва директно в контролера (без view) заглавие и съдържание: 
+  любим ваш десерт и негова рецепта със снимка.
+*/
+
+	class Desert extends CI_Controller 
+	{
+		public function serve()
+		{
+			echo "My favorite desert is cake!<br/>";
+			echo "<img style='width:150px' src='http://calendars-birthdays.com/wp-content/uploads/2015/04/Birthday-Cake-Candles.jpeg'>";
+			echo "<br/> Recipes:";
+			echo "<ul>
+					<li>flour</li>
+					<li>sugar</li>
+					<li>cherry</li>
+					<li>milk</li>
+					<li>eggs</li>
+				</ul>
+			";
+		}
+	}
+?>
